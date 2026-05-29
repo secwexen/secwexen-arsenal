86d8382322f1b37d90f3224723b665ed966bbe24
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-05-29 20:08:07 +0300

    Update validators.py

--- utils/validators.py
+++ utils/validators.py
@@ -4,7 +4,6 @@ def is_valid_domain(domain: str) -> bool:
     pattern = r"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.[A-Za-z]{2,6}$"
     return re.match(pattern, domain) is not None
 
-
 def is_valid_ip(ip: str) -> bool:
     pattern = r"^(\d{1,3}\.){3}\d{1,3}$"
     if not re.match(pattern, ip):
@@ -12,7 +11,6 @@ def is_valid_ip(ip: str) -> bool:
 
     parts = ip.split(".")
     return all(0 <= int(part) <= 255 for part in parts)
-
 
 def is_valid_email(email: str) -> bool:
     pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
