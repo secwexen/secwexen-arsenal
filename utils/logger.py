5f5df6dfc171e5875f13650fcc6581c4a6d706f0
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-05-29 20:07:48 +0300

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -3,14 +3,11 @@ from datetime import datetime
 def _timestamp():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-
 def log_info(message: str):
     print(f"[INFO] [{_timestamp()}] {message}")
 
-
 def log_warning(message: str):
     print(f"[WARNING] [{_timestamp()}] {message}")
-
 
 def log_error(message: str):
     print(f"[ERROR] [{_timestamp()}] {message}")
