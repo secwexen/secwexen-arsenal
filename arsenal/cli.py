3f89ae7d91ca9506caff38c09881e81345c26c53
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-08-16 11:23:25 +0300

    Remove extra newline in clear_screen function
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- arsenal/cli.py
+++ arsenal/cli.py
@@ -2,7 +2,7 @@ import os
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
-    
+
 def print_banner():
     banner = r"""
    ███████╗███████╗ ██████╗██╗    ██╗███████╗██╗  ██╗███████╗███╗   ██╗
